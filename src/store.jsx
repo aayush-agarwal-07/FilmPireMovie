46d76e1dcd7b53eb0f435d081ab9c999af5e227d
Aayush K Agarwal <aayushagarwal685@gmail.com>
2024-07-08 15:55:56 +0530

    fourth commit

--- src/store.jsx
+++ src/store.jsx
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import personReducer from "./reducers/personSlice";
-import tvReducer from "./reducers/tvSlice";
 import movieReducer from "./reducers/movieSlice";
+import tvReducer from "./reducers/tvSlice";
 export const store = configureStore({
   reducer: {
     movie: movieReducer,
     tv: tvReducer,
-    person: personReducer,
   },
 });
