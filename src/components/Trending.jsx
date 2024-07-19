451be5f829f6ff7c7cf300ec1913d7284d2f6f94
Aayush K Agarwal <aayushagarwal685@gmail.com>
2024-07-19 11:48:22 +0530

    dropdown

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -80,7 +80,7 @@ const Trending = () => {
         <h1 className="text-2xl font-semibold text-zinc-400 absolute top-[7%] left-[7%]">
           Trending
         </h1>
-        <div className="w-[70%] ml-[10%] z-[10%]">
+        <div className="w-[62%] ml-[10%] z-[10%]">
           <Topnav />
         </div>
         <Dropdown
